f137f43dd7187edd648b66bde47bf2c36fab7cd6
MatheusDemiro <33012830+MatheusDemiro@users.noreply.github.com>
2018-05-02 15:04:51 -0300

    projeto inicial

---
+++ Zstok/app/src/main/java/com/zstok/endereco/dominio/Endereco.java
@@ -0,0 +1,68 @@
+package com.zstok.endereco.dominio;
+
+public class Endereco {
+    private String rua;
+    private int numero;
+    private String complemento;
+    private String cep;
+    private String bairro;
+    private String municipio;
+    private String uf;
+    private String cidade;
+    private String pais;
+
+    public String getRua() {
+        return rua;
+    }
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String getComplemento() {
+        return complemento;
+    }
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+    public String getCep() {
+        return cep;
+    }
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+    public String getBairro() {
+        return bairro;
+    }
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+    public String getMunicipio() {
+        return municipio;
+    }
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+    public String getUf() {
+        return uf;
+    }
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    public String getPais() {
+        return pais;
+    }
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+}
