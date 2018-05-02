122c1f249759c4a1ec73918037537e2389b6e676
Unknown <mpdemiro@hotmail.com>
2018-05-02 16:52:49 -0300

    projeto inicial

---
+++ Zstok/app/src/main/java/com/zstok/usuario/gui/RegistroActivity.java
@@ -0,0 +1,15 @@
+package com.zstok.usuario.gui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zstok.R;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+}
