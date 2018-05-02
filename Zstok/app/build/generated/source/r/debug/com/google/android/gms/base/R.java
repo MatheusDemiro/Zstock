16a498b3e45fe0239a183d49c435c1f20668fbc6
Unknown <mpdemiro@hotmail.com>
2018-05-02 17:04:36 -0300

    Removi a segunda activity de registro

--- Zstok/app/build/generated/source/r/debug/com/google/android/gms/base/R.java
+++ Zstok/app/build/generated/source/r/debug/com/google/android/gms/base/R.java
@@ -55,12 +55,12 @@ public final class R {
         public static final int adjust_height = 0x7f07001b;
         public static final int adjust_width = 0x7f07001c;
         public static final int auto = 0x7f070021;
-        public static final int dark = 0x7f070034;
-        public static final int icon_only = 0x7f070048;
-        public static final int light = 0x7f070050;
-        public static final int none = 0x7f07005a;
-        public static final int standard = 0x7f070083;
-        public static final int wide = 0x7f070097;
+        public static final int dark = 0x7f070035;
+        public static final int icon_only = 0x7f070049;
+        public static final int light = 0x7f070051;
+        public static final int none = 0x7f07005b;
+        public static final int standard = 0x7f070084;
+        public static final int wide = 0x7f070098;
     }
     public static final class string {
         public static final int common_google_play_services_enable_button = 0x7f0b001e;
