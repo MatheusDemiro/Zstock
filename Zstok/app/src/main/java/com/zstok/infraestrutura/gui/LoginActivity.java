2ce18d2d26831cd20335e5d4440151415ef8e468
Unknown <mpdemiro@hotmail.com>
2018-05-03 22:59:19 -0300

    Implementação parcial das telas de login e registro

--- Zstok/app/src/main/java/com/zstok/infraestrutura/gui/LoginActivity.java
+++ Zstok/app/src/main/java/com/zstok/infraestrutura/gui/LoginActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.zstok.R;
+import com.zstok.infraestrutura.utils.VerificaConexao;
 import com.zstok.pessoa.gui.RegistroActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,14 +17,21 @@ public class LoginActivity extends AppCompatActivity {
     private EditText edtEmail;
     private EditText edtSenha;
 
+    private VerificaConexao verificaConexao;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        //Instanciando views
         edtEmail = findViewById(R.id.edtEmail);
         edtSenha = findViewById(R.id.edtSenha);
         TextView tvRegistreSe = findViewById(R.id.tvRegistreSe);
+        Button btnEntrar = findViewById(R.id.btnEntrar);
+
+        //Inicializando a instância da classe VerificaConexao
+        verificaConexao = new VerificaConexao(this);
 
         tvRegistreSe.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,7 +40,33 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (verificaConexao.isConected()) {
+                    if (validarCampos()) {
+                        //Sign in Usuário
+                        //Fazer consulta na árvore e verificar a validez do email
+                    }
+                }
+            }
+        });
     }
+    //Validando views
+    private boolean validarCampos(){
+        boolean verificador = true;
+
+        if (edtEmail.getText().toString().trim().isEmpty()){
+            edtEmail.setError(getString(R.string.zs_excecao_campo_vazio));
+            verificador = false;
+        }
+        if (edtSenha.getText().toString().trim().isEmpty()){
+            edtSenha.setError(getString(R.string.zs_excecao_campo_vazio));
+            verificador = false;
+        }
+        return verificador;
+    }
+    //Transição para a tela de registro
     private void abrirTelaRegistro(){
         Intent intent = new Intent(getApplicationContext(), RegistroActivity.class);
         startActivity(intent);
--- Zstok/app/src/main/java/com/zstok/usuario/gui/RegistroActivity.java
+++ Zstok/app/src/main/java/com/zstok/usuario/gui/RegistroActivity.java
@@ -1,15 +1,122 @@
 package com.zstok.usuario.gui;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.zstok.R;
+import com.zstok.infraestrutura.persistencia.FirebaseController;
+import com.zstok.infraestrutura.utils.VerificaConexao;
 
 public class RegistroActivity extends AppCompatActivity {
 
+    private EditText edtRegCpfCnpj;
+    private EditText edtRegEmail;
+    private EditText edtRegSenha;
+
+    private VerificaConexao verificaConexao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        //Instâncias das views
+        edtRegCpfCnpj = findViewById(R.id.edtRegistroCpfCnpj);
+        edtRegEmail = findViewById(R.id.edtRegistroEmail);
+        edtRegSenha = findViewById(R.id.edtRegistroSenha);
+        Button btnCadastrar = findViewById(R.id.btnCadastrar);
+
+        //Inicializando a instância da classe VerificaConexao
+        verificaConexao = new VerificaConexao(this);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (verificaConexao.isConected()){
+                    if (validarCampos()){
+                        cadastrarUsuario();
+                    }
+                }
+            }
+        });
+
+    }
+    //Autenticando usuário
+    private void cadastrarUsuario(){
+        final FirebaseAuth autenticacao = FirebaseController.getFirebaseAuthentication();
+        autenticacao.createUserWithEmailAndPassword(
+                edtRegEmail.getText().toString(),
+                edtRegSenha.getText().toString()
+        ).addOnCompleteListener(RegistroActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (verificaAutenticacao(task)){
+                    //Insere no banco (Negócio -> Persistência)
+                }
+            }
+        });
+    }
+    //Verificando autenticação
+    private boolean verificaAutenticacao(@NonNull Task<AuthResult> task) {
+        boolean verificador = true;
+
+        try{
+            if (task.isSuccessful()) {
+                verificador = true;
+            }else {
+                verificador = false;
+                throw task.getException();
+            }
+        }catch (FirebaseAuthWeakPasswordException e){
+            edtRegSenha.setError(getString(R.string.zs_excecao_senha_forte));
+        }catch (FirebaseAuthInvalidCredentialsException e){
+            edtRegEmail.setError(getString(R.string.zs_excecao_email));
+        }catch (FirebaseAuthUserCollisionException e){
+            edtRegEmail.setError(getString(R.string.zs_excecao_email));
+        }catch (Exception e){
+            Toast.makeText(RegistroActivity.this, getString(R.string.zs_excecao_database), Toast.LENGTH_LONG).show();
+        }
+        return verificador;
+    }
+    //Validando EditTexts
+    private boolean validarCampos(){
+        boolean verificador = true;
+
+        if (edtRegCpfCnpj.getText().toString().trim().isEmpty()){
+            edtRegCpfCnpj.setError(getString(R.string.zs_excecao_campo_vazio));
+            verificador = false;
+        }
+        if (edtRegCpfCnpj.getText().toString().trim().length() > 11 && edtRegCpfCnpj.getText().toString().trim().length() < 14){
+            edtRegCpfCnpj.setError(getString(R.string.zs_excecao_cpf_cnpj));
+            verificador = false;
+        }
+        if (edtRegCpfCnpj.getText().toString().trim().length() < 11 && edtRegCpfCnpj.getText().toString().trim().length() > 14){
+            edtRegCpfCnpj.setError(getString(R.string.zs_excecao_cpf_cnpj));
+            verificador = false;
+        }
+        if (edtRegEmail.getText().toString().trim().isEmpty()){
+            edtRegEmail.setError(getString(R.string.zs_excecao_campo_vazio));
+            verificador = false;
+        }
+        if (edtRegSenha.getText().toString().trim().isEmpty()){
+            edtRegSenha.setError(getString(R.string.zs_excecao_campo_vazio));
+            verificador = false;
+        }
+        if (edtRegSenha.getText().toString().length() < 6){
+            edtRegSenha.setError(getString(R.string.zs_excecao_senha_forte));
+            verificador = false;
+        }
+        return verificador;
     }
 }
